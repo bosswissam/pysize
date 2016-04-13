f93942ec6f74fd30f759005106c18fb72bee5de3
Wissam Jarjoui <bosswissam@gmail.com>
2016-04-13 01:35:21 -0700

    added helper to compute full size of an object

---
+++ pysize.py
@@ -0,0 +1,15 @@
+import sys
+
+def get_size(obj):
+    ''' Recursively finds size of objects'''
+    size = 0
+    if isinstance(obj, dict):
+        size+= sum([get_size(v) for v in obj.values()])
+        size+= sum([get_size(k) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size+= get_size(obj.__dict__)
+    elif hasattr(obj, '__iter__'):
+        size+=sum([get_size(i) for i in obj])
+    else:
+        size+=sys.getsizeof(obj)
+    return size
