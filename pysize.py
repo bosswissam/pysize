884ffca4772891c0ffe6c0f67ae6b012ed2ec5f9
Wissam Jarjoui <wissam@goshippo.com>
2016-07-21 14:07:52 -0700

    Added handling for repeated and self-referential objects + Tests

--- pysize.py
+++ pysize.py
@@ -1,13 +1,21 @@
 import sys
 
-def get_size(obj):
+def get_size(obj, seen=None):
     """Recursively finds size of objects"""
     size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
     if isinstance(obj, dict):
-        size += sum([get_size(v) for v in obj.values()])
-        size += sum([get_size(k) for k in obj.keys()])
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
     elif hasattr(obj, '__dict__'):
-        size += get_size(obj.__dict__)
+        size += get_size(obj.__dict__, seen)
     elif hasattr(obj, '__iter__'):
-        size += sum([get_size(i) for i in obj])
+        size += sum([get_size(i, seen) for i in obj])
     return size
---
+++ test_pysize.py
@@ -0,0 +1,40 @@
+import sys
+import unittest
+
+import pysize
+
+class TestPysize(unittest.TestCase):
+    def test_empty_list(self):
+        empty_list = []
+        self.assertEqual(sys.getsizeof(empty_list),
+                         pysize.get_size(empty_list))
+
+    def test_list_of_collections(self):
+        collection_list = [[], {}, ()]
+        pointer_byte_size = 8*len(collection_list)
+        empty_list_size = sys.getsizeof([])
+        empty_tuple_size = sys.getsizeof(())
+        empty_dict_size = sys.getsizeof({})
+        expected_size = empty_list_size * 2 + empty_tuple_size +\
+                        empty_dict_size + pointer_byte_size
+
+        self.assertEqual(expected_size,
+                         pysize.get_size(collection_list))
+
+    def test_no_double_counting(self):
+        rep = ["test1"]
+        obj = [rep, rep]
+        obj2 = [rep]
+        self.assertEqual(pysize.get_size(obj), pysize.get_size(obj2) + 8)
+
+    def test_gracefully_handles_self_referential_objects(self):
+        class Test(object):
+            pass
+        obj = Test()
+        obj.prop = obj
+        obj2 = Test()
+        self.assertEqual(pysize.get_size(obj), pysize.get_size(obj.prop))
+
+
+if __name__ == '__main__':
+    unittest.main()
