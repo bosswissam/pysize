755c77c0cb646bec3fe69897dc1fcb2a53f56d2f
Gabe Appleton <gappleto97+development@gmail.com>
2016-07-21 15:43:33 -0700

    Generator-ify list comprehension

--- pysize.py
+++ pysize.py
@@ -12,10 +12,10 @@ def get_size(obj, seen=None):
     # self-referential objects
     seen.add(obj_id)
     if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
+        size += sum((get_size(v, seen) for v in obj.values()))
+        size += sum((get_size(k, seen) for k in obj.keys()))
     elif hasattr(obj, '__dict__'):
         size += get_size(obj.__dict__, seen)
     elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
+        size += sum((get_size(i, seen) for i in obj))
     return size
